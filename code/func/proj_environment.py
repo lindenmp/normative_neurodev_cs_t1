f4de5515fa4ba755e172602d4e7e7d709a7137b5
Linden Parkes <lindenparkes@gmail.com>
2020-01-09 17:04:19 -0500

    Stripped out diffusion directories

--- code/func/proj_environment.py
+++ code/func/proj_environment.py
@@ -7,11 +7,11 @@ import os, sys
 import numpy as np
 
 def set_proj_env(dataset = 'PNC', train_test_str = 'squeakycleanExclude', exclude_str = 't1Exclude',
-    parc_str = 'schaefer', parc_scale = 400, parc_variant = 'orig', edge_weight = 'streamlineCount',
+    parc_str = 'schaefer', parc_scale = 400, parc_variant = 'orig',
     primary_covariate = 'ageAtScan1_Years', extra_str = ''):
 
     # Project root directory
-    projdir = '/Users/lindenmp/Dropbox/Work/ResProjects/NormativeNeuroDev_CrossSec'; os.environ['PROJDIR'] = projdir
+    projdir = '/Users/lindenmp/Dropbox/Work/ResProjects/NormativeNeuroDev_CrossSec_T1'; os.environ['PROJDIR'] = projdir
     
     # Derivatives for dataset --> root directory for the dataset under analysis
     derivsdir = os.path.join('/Users/lindenmp/Dropbox/Work/ResData/PNC/'); os.environ['DERIVSDIR'] = derivsdir
@@ -36,37 +36,20 @@ def set_proj_env(dataset = 'PNC', train_test_str = 'squeakycleanExclude', exclud
         num_parcels = parcel_names.shape[0]
 
     # Cortical thickness directory
-    ctdir = os.path.join(derivsdir, 'processedData/antsCorticalThickness_inMNI'); os.environ['CTDIR'] = ctdir
+    ctdir = os.path.join(derivsdir, 'processedData/voxelwiseMaps_antsCt'); os.environ['CTDIR'] = ctdir
+    gmddir = os.path.join(derivsdir, 'processedData/voxelwiseMaps_gmd'); os.environ['GMDDIR'] = gmddir
 
-    if parc_str == 'lausanne':
-        # cortical thickness text file name
-        ct_file_name = 'ct_' + parc_str + str(parc_scale) + '.txt'; os.environ['CT_FILE_NAME'] = ct_file_name
-        
-        # Structural connectivity derivatives
-        scdir = os.path.join(derivsdir, 'processedData/diffusion/deterministic_dec2016', edge_weight, 'LausanneScale' + str(parc_scale)); os.environ['SCDIR'] = scdir
-
-        # template file name for a subject's structural connectivity .mat file (sourced from Ted's group)
-        # this is a template because it requires some find and replacement -- see compute_node_metric.ipynb
-        sc_name_tmp = 'scanid_' + edge_weight + '_LausanneScale' + str(parc_scale) + '.mat'; os.environ['SC_NAME_TMP'] = sc_name_tmp
-        
-        # field inside .mat file to reference to get the A matrix
-        if edge_weight == 'streamlineCount': os.environ['CONN_STR'] = 'connectivity'
-        elif edge_weight == 'volNormStreamline': os.environ['CONN_STR'] = 'volNorm_connectivity'
-    elif parc_str == 'schaefer':
-        ct_file_name = 'ct_' + parc_str + str(parc_scale) + '_17.txt'; os.environ['CT_FILE_NAME'] = ct_file_name
-        
-        scdir = os.path.join(derivsdir, 'processedData/diffusion/deterministic_20171118'); os.environ['SCDIR'] = scdir
-        sc_name_tmp = 'bblid/*xscanid/tractography/connectivity/bblid_*xscanid_SchaeferPNC_' + str(parc_scale) + '_dti_streamlineCount_connectivity.mat'; os.environ['SC_NAME_TMP'] = sc_name_tmp
-
-        os.environ['CONN_STR'] = 'connectivity'
+    if parc_str == 'schaefer':
+        ct_name_tmp = 'scanid_CorticalThicknessNormalizedToTemplate2mm_schaefer' + str(parc_scale) + '_17.txt'; os.environ['CT_NAME_TMP'] = ct_name_tmp
+        gmd_name_tmp = 'scanid_atropos3class_prob02SubjToTemp2mm_schaefer' + str(parc_scale) + '_17.txt'; os.environ['GMD_NAME_TMP'] = gmd_name_tmp
 
     # Normative dir based on the train/test split --> specific combinations of parcellation/number of parcels/edge weight come off this directory
     # This is the first of the output directories for the project and is created by get_train_test.ipynb if it doesn't exist
     trtedir = os.path.join(projdir, 'analysis/normative', exclude_str, train_test_str); os.environ['TRTEDIR'] = trtedir
 
     # Subdirector in normative dir (TRTEDIR) that designates combination of parcellation/number of parcels/edge weight
-    modeldir_base = os.path.join(trtedir, parc_str + '_' + str(num_parcels) + '_' + edge_weight); os.environ['MODELDIR_BASE'] = modeldir_base
-    modeldir = os.path.join(trtedir, parc_str + '_' + str(num_parcels) + '_' + edge_weight + extra_str); os.environ['MODELDIR'] = modeldir
+    modeldir_base = os.path.join(trtedir, parc_str + '_' + str(num_parcels)); os.environ['MODELDIR_BASE'] = modeldir_base
+    modeldir = os.path.join(trtedir, parc_str + '_' + str(num_parcels) + extra_str); os.environ['MODELDIR'] = modeldir
 
     # normative dir
     model = primary_covariate + '+sex_adj'
