12edf4734f9f85c855ab29615e2179ba1c18d4d0
Linden Parkes <lindenparkes@gmail.com>
2020-02-12 09:47:22 -0500

    Remove outlier detection stuff. Not needed for T1 metrics.

--- code/clean_node_metrics.py
+++ code/clean_node_metrics.py
@@ -97,7 +97,7 @@ for metric in metrics:
 metric_x = 'ageAtScan1'
 metric_y = 'ct'
 f = sns.jointplot(x = df[metric_x], y = df_node_mean[metric_y], kind="reg")
-f.annotate(sp.stats.spearmanr)
+f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
 f.ax_joint.collections[0].set_alpha(0)
 
@@ -108,7 +108,7 @@ f.ax_joint.collections[0].set_alpha(0)
 metric_x = 'ageAtScan1'
 metric_y = 'jd'
 f = sns.jointplot(x = df[metric_x], y = df_node_mean[metric_y], kind="reg")
-f.annotate(sp.stats.spearmanr)
+f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
 f.ax_joint.collections[0].set_alpha(0)
 
@@ -116,61 +116,12 @@ f.ax_joint.collections[0].set_alpha(0)
 # In[12]:
 
 
-my_thresh = 100
-
-
-# In[13]:
-
-
-for metric in metrics:
-    x = df_node_mean.loc[:,metric].copy()
-    outliers = mark_outliers(x, thresh = my_thresh)
-    print(metric + ': ' + str(np.round((outliers.sum() / x.shape[0]) * 100,2)))
-
-
-# Setting threshold to 100 basically 'skips' this step - pretty lazy way of doing it though!
-# Note, results are unaffected by setting my_thresh to 3, 4,5, or 6
-
-# ### Check frequency of outliers
-
-# In[14]:
-
-
-df_node_mask = pd.DataFrame(index = df_node.index, columns = df_node.columns)
-for i, col in enumerate(df_node.columns):
-    x = df_node.loc[:,col].copy()
-    x_out = mark_outliers(x, thresh = my_thresh)
-    df_node_mask.loc[:,col] = x_out
-
-f, axes = plt.subplots(1,2)
-f.set_figwidth(10)
-f.set_figheight(5)
-
-for i, metric in enumerate(metrics):
-    if df_node_mask.filter(regex = metric).sum().any():
-        sns.distplot(df_node_mask.filter(regex = metric).sum()/df_node_mask.filter(regex = metric).shape[0]*100, ax = axes[i])
-
-
-# In[15]:
-
-
-for i, col in enumerate(df_node.columns):
-    x = df_node.loc[:,col].copy()
-    x_out = winsorize_outliers_signed(x, thresh = my_thresh)
-    if np.any(x != x_out):
-        print('winsorizing ' + col)
-        df_node.loc[:,col] = x_out
-
-
-# In[16]:
-
-
 my_str = os.environ['MODELDIR'].split('/')[-1]
 my_str = my_str.split('_')[-1]
 my_str
 
 
-# In[17]:
+# In[13]:
 
 
 df_node.shape
@@ -180,11 +131,11 @@ df_node.shape
 
 # ### cortical thickness
 
-# In[18]:
+# In[14]:
 
 
 # labels of nuisance regressors
-nuis = ['mprage_antsCT_vol_TBV','averageManualRating']
+nuis = ['averageManualRating']
 print(nuis)
 df_nuis = df[nuis]
 df_nuis = sm.add_constant(df_nuis)
@@ -198,13 +149,13 @@ y_pred.columns = cols
 df_node.loc[:,cols] = df_node.loc[:,cols] - y_pred
 
 
-# In[19]:
+# In[15]:
 
 
 df_node.head()
 
 
-# In[20]:
+# In[16]:
 
 
 df_node_mean = pd.DataFrame(index = df_node.index, columns = metrics)
@@ -212,40 +163,40 @@ for metric in metrics:
     df_node_mean[metric] = df_node.filter(regex = metric, axis = 1).mean(axis = 1)
 
 
-# In[21]:
+# In[17]:
 
 
 metric_x = 'ageAtScan1_Years'
 metric_y = 'ct'
 f = sns.jointplot(x = df[metric_x], y = df_node_mean[metric_y], kind="reg")
-f.annotate(sp.stats.spearmanr)
+f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
 f.ax_joint.collections[0].set_alpha(0)
 
 
-# In[22]:
+# In[18]:
 
 
 metric_x = 'ageAtScan1_Years'
 metric_y = 'jd'
 f = sns.jointplot(x = df[metric_x], y = df_node_mean[metric_y], kind="reg")
-f.annotate(sp.stats.spearmanr)
+f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
 f.ax_joint.collections[0].set_alpha(0)
 
 
-# In[23]:
+# In[19]:
 
 
 metric_x = 'ct'
 metric_y = 'jd'
 f = sns.jointplot(x = df_node_mean[metric_x], y = df_node_mean[metric_y], kind="reg")
-f.annotate(sp.stats.spearmanr)
+f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
 f.ax_joint.collections[0].set_alpha(0)
 
 
-# In[24]:
+# In[20]:
 
 
 R = np.zeros(num_parcels-1)
@@ -253,49 +204,46 @@ for i in range(0,num_parcels-1):
     R[i] = sp.stats.pearsonr(df_node.loc[:,'ct_'+str(i)],df_node.loc[:,'jd_'+str(i)])[0]
 
 
-# In[25]:
+# In[21]:
 
 
 sns.distplot(R)
 
 
-# In[26]:
+# In[22]:
 
 
 f = sns.jointplot(x = df_node['ct_'+str(np.argmax(R))], y = df_node['jd_'+str(np.argmax(R))], kind="reg")
-f.annotate(sp.stats.spearmanr)
-# f.annotate(sp.stats.pearsonr)
+f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
 f.ax_joint.collections[0].set_alpha(0)
 
 
-# In[27]:
+# In[23]:
 
 
 f = sns.jointplot(x = df_node['ct_'+str(np.argmin(R))], y = df_node['jd_'+str(np.argmin(R))], kind="reg")
-f.annotate(sp.stats.spearmanr)
-# f.annotate(sp.stats.pearsonr)
+f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
 f.ax_joint.collections[0].set_alpha(0)
 
 
-# In[28]:
+# In[24]:
 
 
 val = np.min(np.abs(R))
 
 
-# In[29]:
+# In[25]:
 
 
 f = sns.jointplot(x = df_node['ct_'+str(np.where(R == val)[0][0])], y = df_node['jd_'+str(np.where(R == val)[0][0])], kind="reg")
-f.annotate(sp.stats.spearmanr)
-# f.annotate(sp.stats.pearsonr)
+f.annotate(sp.stats.pearsonr)
 f.plot_joint(plt.scatter, c = "k", s = 5, linewidth = 2, marker = ".", alpha = 0.3)
 f.ax_joint.collections[0].set_alpha(0)
 
 
-# In[30]:
+# In[26]:
 
 
 df_node.isna().any().any()
@@ -303,7 +251,7 @@ df_node.isna().any().any()
 
 # ## Save out
 
-# In[31]:
+# In[27]:
 
 
 # Save out
