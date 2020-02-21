db698a25a037404c9b1ef8d46f6aad3a4b300e47
Linden Parkes <lindenparkes@gmail.com>
2020-02-21 12:14:16 -0500

    Update of results for biol psych submission

--- code/results_s3.py
+++ code/results_s3.py
@@ -13,12 +13,14 @@ import seaborn as sns
 import scipy as sp
 from scipy import stats
 import matplotlib.pyplot as plt
+plt.rcParams['svg.fonttype'] = 'none'
+import statsmodels.api as sm
 
 
 # In[2]:
 
 
-sys.path.append('/Users/lindenmp/Dropbox/Work/ResProjects/NormativeNeuroDev_CrossSec/code/func/')
+sys.path.append('/Users/lindenmp/Dropbox/Work/ResProjects/NormativeNeuroDev_CrossSec_T1/code/func/')
 from proj_environment import set_proj_env
 from func import run_corr, get_fdr_p, get_cmap, perc_dev, get_fdr_p_df
 
@@ -28,7 +30,7 @@ from func import run_corr, get_fdr_p, get_cmap, perc_dev, get_fdr_p_df
 
 train_test_str = 'squeakycleanExclude' # 'squeakycleanExclude' 'trte_psychopathology'
 exclude_str = 't1Exclude' # 't1Exclude' 'fsFinalExclude'
-parc_str = 'schaefer' # 'schaefer' 'lausanne'
+parc_str = 'schaefer'
 parc_scale = 400
 primary_covariate = 'ageAtScan1_Years'
 parcel_names, parcel_loc, drop_parcels, num_parcels, yeo_idx, yeo_labels = set_proj_env(train_test_str = train_test_str, exclude_str = exclude_str,
@@ -44,7 +46,7 @@ os.environ['NORMATIVEDIR']
 # In[5]:
 
 
-metrics = ('ct', 'str', 'ac', 'mc')
+metrics = ('ct', 'vol')
 phenos = ('Overall_Psychopathology','Psychosis_Positive','Psychosis_NegativeDisorg','AnxiousMisery','Externalizing','Fear')
 
 
@@ -71,32 +73,33 @@ df_node.set_index(['bblid', 'scanid'], inplace = True)
 # In[7]:
 
 
+# regress out sex
+df_nuis = df_train.loc[:,'sex_adj']
+df_nuis = sm.add_constant(df_nuis)
+
+cols = df_node_train.columns
+mdl = sm.OLS(df_node_train.loc[:,cols].astype(float), df_nuis.astype(float)).fit()
+y_pred = mdl.predict(df_nuis)
+y_pred.columns = cols
+df_node_train.loc[:,cols] = df_node_train.loc[:,cols] - y_pred
+
+
+# In[8]:
+
+
 # age effect on training set
 df_age_effect = run_corr(df_train[primary_covariate], df_node_train, typ = 'spearmanr'); df_age_effect['p_fdr'] = get_fdr_p(df_age_effect['p'])
-if parc_str == 'lausanne':
-    df_age_effect.drop(my_list, axis = 0, inplace = True)
 age_alpha = 0.05
 age_filter = df_age_effect['p_fdr'].values < age_alpha
 
 
-# In[8]:
+# In[9]:
 
 
 age_filter.sum()
 
 
 # ## Load nispat outputs
-
-# In[9]:
-
-
-# Forward model
-synth_cov_test = pd.read_csv(os.path.join(os.environ['NORMATIVEDIR'], 'forward/synth_cov_test.txt'),
-                             delim_whitespace = True, names=[primary_covariate, 'sex_adj'])
-
-yhat_forward = np.loadtxt(os.path.join(os.environ['NORMATIVEDIR'], 'forward/yhat.txt'), delimiter = ' ').transpose()
-df_yhat_forward = pd.DataFrame(data = yhat_forward, index = synth_cov_test.index, columns = df_node.columns)
-
 
 # In[10]:
 
@@ -138,15 +141,11 @@ region_filter.sum()
 # In[14]:
 
 
-# at each roi, the differences between first and last age point in the synthetic data
-# for each node/metric, a negative value means that the nm predicted an overall decrease in y with age (i.e, a negative function),
-# while a positive values means that the nm predicted an overall increase in y with age (i.e., a positive function)
-df_yhat_tmp1 = df_yhat_forward[synth_cov_test['sex_adj'] == 0].iloc[-1,:] - df_yhat_forward[synth_cov_test['sex_adj'] == 0].iloc[0,:]
-df_yhat_tmp2 = df_yhat_forward[synth_cov_test['sex_adj'] == 1].iloc[-1,:] - df_yhat_forward[synth_cov_test['sex_adj'] == 1].iloc[0,:]
-df_yhat_diff = pd.concat((df_yhat_tmp1, df_yhat_tmp2), axis = 1)
-
 # boolean that designates which regions carry with positive predicted change.
-nm_is_pos = df_yhat_diff[0] > 0
+nm_is_pos = df_age_effect['coef'] > 0
+print('Percentage of regions flipp by metric')
+for metric in metrics:
+    print(metric + ': ' + str(nm_is_pos.filter(regex = metric).sum() / num_parcels * 100))
 
 # flipping the z-stats in these regions has the effect of standardising their interpration across the brain to be inline
 # with the negative predicted change statement above
@@ -168,9 +167,16 @@ df_pheno_z.set_index(['pheno','node'], inplace = True)
 print(str(np.sum(df_pheno_z['sig_age_smse'])) + ' significant effects (fdr)')
 
 
+# In[17]:
+
+
+for pheno in phenos:
+    print(pheno + ': ' + str(np.sum(df_pheno_z.loc[pheno]['sig_age_smse'] == True)) + ' significant effects (fdr)')
+
+
 # # Plots
 
-# In[17]:
+# In[18]:
 
 
 if not os.path.exists(os.environ['FIGDIR']): os.makedirs(os.environ['FIGDIR'])
@@ -181,9 +187,9 @@ cmap = get_cmap('pair')
 phenos = ('Overall_Psychopathology','Psychosis_Positive','Psychosis_NegativeDisorg','AnxiousMisery','Externalizing','Fear')
 phenos_label_short = ('Ov. Psych.', 'Psy. (pos.)', 'Psy. (neg.)', 'Anx.-mis.', 'Ext.', 'Fear')
 phenos_label = ('Overall Psychopathology','Psychosis (Positive)','Psychosis (Negative)','Anxious-Misery','Externalizing','Fear')
-metrics = ('ct', 'str', 'ac', 'mc')
-metrics_label_short = ('Thickness', 'Strength', 'Ave. ctrb.', 'Mod. ctrb.')
-metrics_label = ('Thickness', 'Strength', 'Average controllability', 'Modal controllability')
+metrics = ('ct', 'vol')
+metrics_label_short = ('Thickness', 'Volume')
+metrics_label = ('Thickness', 'Volume')
 
 print(phenos)
 print(metrics)
@@ -196,7 +202,7 @@ for metric in metrics:
 
 # ### Calculate node summary metrics
 
-# In[18]:
+# In[19]:
 
 
 z_thr = 2.6    
@@ -205,119 +211,75 @@ node_z_perc_p = perc_dev(df_z.transpose(), thr = z_thr, sign = 'pos')
 node_z_perc_n = perc_dev(df_z.transpose(), thr = z_thr, sign = 'neg')
 
 
-# In[19]:
+# In[20]:
 
 
 for metric in metrics:
     roi_data = node_z_perc_p[region_filter].filter(regex = metric)
     print('max +ve % for ' + metric + ': ' + str(np.round(roi_data.max(),2)))
+    print('count of +ve deviations >2% for ' + metric + ': ' + str(np.sum(roi_data > 2)))
+    print('count of +ve deviations >1.5% for ' + metric + ': ' + str(np.sum(roi_data > 1.5)))
     roi_data = node_z_perc_n[region_filter].filter(regex = metric)
     print('max -ve % for ' + metric + ': ' + str(np.round(roi_data.max(),2)))
+    print('count of -ve deviations >2% for ' + metric + ': ' + str(np.sum(roi_data > 2)))
+    print('count of -ve deviations >1.5% for ' + metric + ': ' + str(np.sum(roi_data > 1.5)))
     print()
-
-
-# In[20]:
-
-
-pos_counts = pd.DataFrame(index = metrics, columns = phenos[1:])
-neg_counts = pd.DataFrame(index = metrics, columns = phenos[1:])
-stat_ttest = pd.DataFrame(index = metrics, columns = phenos[1:])
-stat_ttest_p = pd.DataFrame(index = metrics, columns = phenos[1:])
-which_ttest = pd.DataFrame(index = metrics, columns = phenos[1:])
-
-
-for pheno in phenos[1:]:
-    for metric in metrics:
-#         pheno_bol = df_pheno_z.loc[pheno,'sig_age'].filter(regex = metric)
-        pheno_bol = df_pheno_z.loc[pheno,'sig_age_smse'].filter(regex = metric)
-        if pheno_bol.sum() > 1:
-            x = node_z_perc_p.filter(regex = metric)[pheno_bol]
-            y = node_z_perc_n.filter(regex = metric)[pheno_bol]
-
-            if np.mean(x) == 0 or np.mean(y) == 0:
-                if np.var(x) == 0 or np.mean(x) == 0:
-                    stat_ttest.loc[metric,pheno] = sp.stats.ttest_1samp(y, popmean = 0)[0]
-                    stat_ttest_p.loc[metric,pheno] = sp.stats.ttest_1samp(y, popmean = 0)[1]
-                    which_ttest.loc[metric,pheno] = 'sinT'
-                elif np.var(y) == 0 or np.mean(y) == 0:
-                    stat_ttest.loc[metric,pheno] = sp.stats.ttest_1samp(x, popmean = 0)[0]
-                    stat_ttest_p.loc[metric,pheno] = sp.stats.ttest_1samp(x, popmean = 0)[1]
-                    which_ttest.loc[metric,pheno] = 'sinT'
-            else:   
-                stat_ttest.loc[metric,pheno] = sp.stats.ttest_rel(x,y)[0]
-                stat_ttest_p.loc[metric,pheno] = sp.stats.ttest_rel(x,y)[1]
-                which_ttest.loc[metric,pheno] = 'repT'
-
-            pos_counts.loc[metric,pheno] = x.mean()
-            neg_counts.loc[metric,pheno] = y.mean()
-            
-stat_ttest_p_fdr = get_fdr_p_df(stat_ttest_p)
 
 
 # In[21]:
 
 
-which_ttest
+for metric in metrics:
+    r = sp.stats.spearmanr(node_z_perc_p[region_filter].filter(regex = metric), node_z_perc_n[region_filter].filter(regex = metric))
+    if r[1] < .001:
+        print(metric + ': ' + str(np.round(r[0],2)) + ', ' + str(np.round(r[1],3)))
 
 
 # In[22]:
 
 
-pos_counts > neg_counts
+r = pd.DataFrame(index = metrics, columns = phenos)
+p = pd.DataFrame(index = metrics, columns = phenos)
+
+for pheno in phenos:
+    for metric in metrics:
+        pheno_data = df_pheno_z.loc[pheno].loc[region_filter,'coef'].filter(regex = metric)
+        roi_data = node_z_perc_p[region_filter].filter(regex = metric)
+#         roi_data = node_z_perc_n[region_filter].filter(regex = metric)
+        r.loc[metric,pheno] = sp.stats.spearmanr(pheno_data, roi_data)[0]
+        p.loc[metric,pheno] = sp.stats.spearmanr(pheno_data, roi_data)[1]
+p = get_fdr_p_df(p, alpha = 0.05)
 
 
 # In[23]:
 
 
-stat_ttest[stat_ttest_p_fdr < 0.05]
+r[p<.05]
 
-
-# Figure 5
 
 # In[24]:
 
 
-f, ax = plt.subplots()
-f.set_figwidth(2)
-f.set_figheight(1.5)
-ax = sns.heatmap(pos_counts.astype(float), center = 0, vmin = 0, vmax = 5.5, annot = True, cmap = 'RdBu_r', cbar_kws={'label': 'Percent EPDs (mean)', 'shrink':1}, square = True, fmt='.1f', annot_kws={"size": 8})
-ax.set_xticklabels(phenos_label_short[1:], rotation = 45, fontsize = 8)
-ax.set_yticklabels(metrics_label_short, rotation = 0, fontsize = 8)
-ax.set_ylabel('')
-f.savefig('epd_count.svg', dpi = 300, bbox_inches = 'tight', pad_inches = 0)
+pheno = 'Fear'
+metric = 'ct'
+pheno_data = df_pheno_z.loc[pheno].loc[region_filter,'coef'].filter(regex = metric)
+roi_data = node_z_perc_p[region_filter].filter(regex = metric)
 
-
-# In[25]:
-
-
-f, ax = plt.subplots()
-f.set_figwidth(2)
-f.set_figheight(1.5)
-ax = sns.heatmap(neg_counts.astype(float), center = 0, vmin = 0, vmax = 5.5, annot = True, cmap = 'RdBu', cbar_kws={'label': 'Percent ENDs (mean)', 'shrink':1}, square = True, fmt='.1f', annot_kws={"size": 8})
-ax.set_xticklabels(phenos_label_short[1:], rotation = 45, fontsize = 8)
-ax.set_yticklabels('', rotation = 0, fontsize = 8)
-ax.set_ylabel('')
-f.savefig('end_count.svg', dpi = 300, bbox_inches = 'tight', pad_inches = 0)
-
-
-# In[26]:
-
-
-f, ax = plt.subplots()
-f.set_figwidth(2)
-f.set_figheight(1.5)
-ax = sns.heatmap(stat_ttest[stat_ttest_p_fdr < 0.05].astype(float), center = 0, vmin = 0, annot = True, cbar_kws={'label': '$\it{t}$ statistics', 'shrink':1}, square = True, fmt='.1f', annot_kws={"size": 8})
-ax.set_xticklabels(phenos_label_short[1:], rotation = 45, fontsize = 8)
-ax.set_yticklabels('', rotation = 0, fontsize = 8)
-ax.set_ylabel('')
-f.savefig('epd-vs-end_ttest.svg', dpi = 300, bbox_inches = 'tight', pad_inches = 0)
+f = sns.jointplot(x = pheno_data, y = roi_data, kind="kde")
+f.fig.set_figwidth(3.25)
+f.fig.set_figheight(3.25)
+f.set_axis_labels('Regional $\\rho$ value', 'Regional percentage of extreme delay')
+f.annotate(sp.stats.spearmanr)
+f.plot_joint(plt.scatter, c = "k", s = 3, linewidth = 2, marker = ".", alpha = 0.25)
+f.ax_joint.collections[0].set_alpha(0)
+f.savefig('corr_spatial_' + pheno + '_' + metric + '_z.svg', dpi = 300, bbox_inches = 'tight', pad_inches = 0)
 
 
 # ## Overlap between individuals
 
 # ### Calculate subject summary metrics
 
-# In[27]:
+# In[25]:
 
 
 # percent of supra-threshold (cf. Wolfers et al. JAMA Psych)
@@ -327,7 +289,7 @@ for metric in metrics:
     df[metric + '_z_perc-n'] = perc_dev(df_z.filter(regex = metric), thr = z_thr, sign = 'neg')
 
 
-# In[28]:
+# In[26]:
 
 
 pos_bol = df['ct_z_perc-p'] > 0 # vectors of people with non-zero 0 percentages extreme positive deviations
@@ -335,49 +297,30 @@ neg_bol = df['ct_z_perc-n'] > 0 # vectors of people with non-zero 0 percentages 
 sp.spatial.distance.jaccard(pos_bol, neg_bol)
 
 
-# In[29]:
+# In[27]:
 
 
-pos_bol = df['str_z_perc-p'] > 0 # vectors of people with non-zero 0 percentages extreme positive deviations
-neg_bol = df['str_z_perc-n'] > 0 # vectors of people with non-zero 0 percentages extreme negative deviations
-sp.spatial.distance.jaccard(pos_bol, neg_bol)
-
-
-# In[30]:
-
-
-pos_bol = df['ac_z_perc-p'] > 0 # vectors of people with non-zero 0 percentages extreme positive deviations
-neg_bol = df['ac_z_perc-n'] > 0 # vectors of people with non-zero 0 percentages extreme negative deviations
-sp.spatial.distance.jaccard(pos_bol, neg_bol)
-
-
-# In[31]:
-
-
-pos_bol = df['mc_z_perc-p'] > 0 # vectors of people with non-zero 0 percentages extreme positive deviations
-neg_bol = df['mc_z_perc-n'] > 0 # vectors of people with non-zero 0 percentages extreme negative deviations
+pos_bol = df['vol_z_perc-p'] > 0 # vectors of people with non-zero 0 percentages extreme positive deviations
+neg_bol = df['vol_z_perc-n'] > 0 # vectors of people with non-zero 0 percentages extreme negative deviations
 sp.spatial.distance.jaccard(pos_bol, neg_bol)
 
 
 # ## Brain plots nispat
 
-# In[32]:
+# In[28]:
 
 
 import matplotlib.image as mpimg
 from brain_plot_func import roi_to_vtx, brain_plot
 
 
-# In[33]:
+# In[29]:
 
 
-if parc_str == 'schaefer':
-    subject_id = 'fsaverage'
-elif parc_str == 'lausanne':
-    subject_id = 'lausanne125'
+subject_id = 'fsaverage'
 
 
-# In[34]:
+# In[30]:
 
 
 get_ipython().run_line_magic('pylab', 'qt')
@@ -385,7 +328,7 @@ get_ipython().run_line_magic('pylab', 'qt')
 
 # ## Brain plots - overlap maps
 
-# In[35]:
+# In[31]:
 
 
 for metric in metrics:
@@ -402,26 +345,30 @@ for metric in metrics:
                 fig_str = hemi + '_' + metric + '_z_perc_n'
                 roi_data = node_z_perc_n.filter(regex = metric).values
 
-            if subject_id == 'lausanne125':
-                parc_file = os.path.join('/Applications/freesurfer/subjects/', subject_id, 'label', hemi + '.myaparc_' + str(parc_scale) + '.annot')
-            elif subject_id == 'fsaverage':
-                parc_file = os.path.join('/Users/lindenmp/Dropbox/Work/ResProjects/NeuroDev_NetworkControl/figs/Parcellations/FreeSurfer5.3/fsaverage/label/',
-                                         hemi + '.Schaefer2018_' + str(parc_scale) + 'Parcels_17Networks_order.annot')
+            parc_file = os.path.join('/Users/lindenmp/Dropbox/Work/ResProjects/NeuroDev_NetworkControl/figs/Parcellations/FreeSurfer5.3/fsaverage/label/',
+                                     hemi + '.Schaefer2018_' + str(parc_scale) + 'Parcels_17Networks_order.annot')
+
+            age_filt = df_age_effect.filter(regex = metric, axis = 0)['p_fdr'].values < age_alpha
+            smse_filt = df_smse.filter(regex = metric, axis = 0).values < smse_thresh
+            smse_filt = smse_filt.reshape(-1)
+            region_filt = np.logical_and(age_filt,smse_filt)
 
             # Threshold
             roi_data[roi_data == 0] = -1000
+            roi_data[~region_filt] = -1000
+            if metric == 'ct':
+                center_anchor = 2.5
+            elif metric == 'vol':
+                center_anchor = 2.5
 
             if np.var(roi_data) > 0:
                 # project subject's data to vertices
-                if subject_id == 'lausanne125' and metric == 'ct':
-                    brain_plot(roi_data, parcel_names[parcel_loc == 1], parc_file, fig_str, subject_id = subject_id, hemi = hemi, color = 'hot')
-                else:
-                    brain_plot(roi_data, parcel_names, parc_file, fig_str, subject_id = subject_id, hemi = hemi, color = 'hot')
+                brain_plot(roi_data, parcel_names, parc_file, fig_str, subject_id = subject_id, hemi = hemi, color = 'hot', center_anchor = center_anchor)
             else:
                 print('Nothing significant')
 
 
-# In[36]:
+# In[32]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -429,16 +376,17 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 # Figure 4
 
-# In[37]:
+# In[33]:
 
 
-f, axes = plt.subplots(2, 8)
+f, axes = plt.subplots(2, 4)
 f.set_figwidth(8)
 f.set_figheight(3)
 plt.subplots_adjust(wspace=0, hspace=0)
 
 # column 0:
-fig_str = 'lh_ct_z_perc_n.png'
+hemi = 'lh'
+fig_str = hemi + '_ct_z_perc_p.png'
 try:
 #     axes[0,0].set_title('Thickness (END)')
     image = mpimg.imread('lat_' + fig_str); axes[0,0].imshow(image); axes[0,0].axis('off')
@@ -448,7 +396,7 @@ try:
 except FileNotFoundError: axes[1,0].axis('off')
     
 # column 1:
-fig_str = 'lh_ct_z_perc_p.png'
+fig_str = hemi + '_ct_z_perc_n.png'
 try:
 #     axes[0,1].set_title('Thickness (EPD)')
     image = mpimg.imread('lat_' + fig_str); axes[0,1].imshow(image); axes[0,1].axis('off')
@@ -458,7 +406,7 @@ try:
 except FileNotFoundError: axes[1,1].axis('off')
     
 # column 2:
-fig_str = 'lh_str_z_perc_n.png'
+fig_str = hemi + '_vol_z_perc_p.png'
 try:
 #     axes[0,2].set_title('Strength (END)')
     image = mpimg.imread('lat_' + fig_str); axes[0,2].imshow(image); axes[0,2].axis('off')
@@ -468,7 +416,7 @@ try:
 except FileNotFoundError: axes[1,2].axis('off')
     
 # column 3:
-fig_str = 'lh_str_z_perc_p.png'
+fig_str = hemi + '_vol_z_perc_n.png'
 try:
 #     axes[0,3].set_title('Strength (EPD)')
     image = mpimg.imread('lat_' + fig_str); axes[0,3].imshow(image); axes[0,3].axis('off')
@@ -476,48 +424,8 @@ except FileNotFoundError: axes[0,3].axis('off')
 try:
     image = mpimg.imread('med_' + fig_str); axes[1,3].imshow(image); axes[1,3].axis('off')
 except FileNotFoundError: axes[1,3].axis('off')
-    
-# column 4:
-fig_str = 'lh_ac_z_perc_n.png'
-try:
-#     axes[0,4].set_title('Ave. ctrb. (END)')
-    image = mpimg.imread('lat_' + fig_str); axes[0,4].imshow(image); axes[0,4].axis('off')
-except FileNotFoundError: axes[0,4].axis('off')
-try:
-    image = mpimg.imread('med_' + fig_str); axes[1,4].imshow(image); axes[1,4].axis('off')
-except FileNotFoundError: axes[1,4].axis('off')
-    
-# column 5:
-fig_str = 'lh_ac_z_perc_p.png'
-try:
-#     axes[0,5].set_title('Ave. ctrb. (EPD)')
-    image = mpimg.imread('lat_' + fig_str); axes[0,5].imshow(image); axes[0,5].axis('off')
-except FileNotFoundError: axes[0,5].axis('off')
-try:
-    image = mpimg.imread('med_' + fig_str); axes[1,5].imshow(image); axes[1,5].axis('off')
-except FileNotFoundError: axes[1,5].axis('off')
-    
-# column 6:
-fig_str = 'lh_mc_z_perc_n.png'
-try:
-#     axes[0,6].set_title('Mod. ctrb. (END)')
-    image = mpimg.imread('lat_' + fig_str); axes[0,6].imshow(image); axes[0,6].axis('off')
-except FileNotFoundError: axes[0,6].axis('off')
-try:
-    image = mpimg.imread('med_' + fig_str); axes[1,6].imshow(image); axes[1,6].axis('off')
-except FileNotFoundError: axes[1,6].axis('off')
-    
-# column 7:
-fig_str = 'lh_mc_z_perc_p.png'
-try:
-#     axes[0,7].set_title('Mod. ctrb. (EPD)')
-    image = mpimg.imread('lat_' + fig_str); axes[0,7].imshow(image); axes[0,7].axis('off')
-except FileNotFoundError: axes[0,7].axis('off')
-try:
-    image = mpimg.imread('med_' + fig_str); axes[1,7].imshow(image); axes[1,7].axis('off')
-except FileNotFoundError: axes[1,7].axis('off')
 
 plt.show()
-# f.savefig('brain_plot_tiled_perc_pn.png', dpi = 600, bbox_inches = 'tight', pad_inches = 0)
-f.savefig('brain_plot_tiled_perc_pn.svg', dpi = 1200, bbox_inches = 'tight', pad_inches = 0)
+# f.savefig('brain_plot_tiled_perc_pn_'+hemi+'.png', dpi = 600, bbox_inches = 'tight', pad_inches = 0)
+f.savefig('brain_plot_tiled_perc_pn_'+hemi+'.svg', dpi = 1200, bbox_inches = 'tight', pad_inches = 0)
 
